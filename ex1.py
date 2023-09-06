a4d659fa865a90cbb4c9067f678dd999afa69c57
Saglara <nohaevass@yandex.ru>
2023-09-07 00:45:39 +0300

    1

--- ex1.py
+++ ex1.py
@@ -10,38 +10,50 @@
 # пользователя и произведено перенаправление на страницу
 # ввода имени и электронной почты.
 
-from flask import Flask, request, render_template, redirect, url_for, make_response
+from flask import Flask, request, render_template, redirect, url_for, make_response, session
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+app.secret_key = ''
+#
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+#
+# @app.route('/hello/<name>')
+# def hello(name):
+#     return f'Привет {name}!'
+#
+#
+# @app.get('/submit')
+# def submit_get():
+#     return render_template('form.html')
+#
+#
+# @app.post('/submit')
+# def submit_post():
+#     name = request.form.get('name')
+#     email = request.form.get('email')
+#     # return f'Hello, {name} ({email})!'
+#     response = make_response("Cookie установлен")
+#     response.set_cookie(name, email)
+#     return response
+#     return redirect(url_for('hello', name = name))
+#     # return response
+#     return render_template('form.html')
+#
+@app.route('/log/', methods=['GET', 'POST'])
+def log():
+    if request.method == 'POST':
+        session['username'] = request.form.get('username')
+        return redirect(url_for('index'))
+    return render_template('log.html')
 
-@app.route('/hello/<name>')
-def hello(name):
-    return f'Привет {name}!'
 
-
-@app.get('/submit')
-def submit_get():
-    return render_template('form.html')
-
-
-@app.post('/submit')
-def submit_post():
-    name = request.form.get('name')
-    email = request.form.get('email')
-    # return f'Hello, {name} ({email})!'
-    response = make_response("Cookie установлен")
-    response.set_cookie(name, email)
-    return response
-    return redirect(url_for('hello', name = name))
-    # return response
-    return render_template('form.html')
-
-
-
+@app.route('/logout/')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
